9131bead3a722f03424e79c7693df030ce13a9d6
Sajal Ghosh <86906762+thesajalghosh@users.noreply.github.com>
2025-05-05 01:08:17 +0530

    final

--- src/Component/Avater.jsx
+++ src/Component/Avater.jsx
@@ -460,7 +460,7 @@ const Avater = () => {
                   />
                 </div>
               </div> */}
-              <div className="flex-1 overflow-y-auto p-4">
+              <div className="flex-1 overflow-y-auto p-4 h-[55vh]">
                 {messages.map((message, index) => (
                   <>
                     {message?.text && (
